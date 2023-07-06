0cde7840d5a4264aacde1422676c4a6e0361e992
just-curious12 <manahil1078@hotmail.com>
2023-07-06 21:13:32 +0500

    fetching from met api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState,useEffect} from 'react';
+import React from 'react';
 function App() {
+  const [art,setArt] = useState([])
+  const [artwork,setArtwork] = useState([])
+  useEffect(() => {
+    fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=1700&dateEnd=1800&q=African")
+    .then(res => res.json())
+    .then(data => setArt(data))
+  },[])
+  useEffect(() => {
+    fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/459199")
+    .then(res => res.json())
+    .then(data => setArtwork(data))
+  },[])
+
+  const objects = [art.objectIDs + ","];
+  const artElements = objects.map(element=> {
+    return (
+      <>
+      <div key={element}>{element}</div>
+      </>
+    );
+  });
+  const artworkImage = artwork.primaryImage;
+  
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {artElements}
     </div>
+    <br></br>
+    <img src={artworkImage} width="300" height="300"></img></>
   );
 }
 
