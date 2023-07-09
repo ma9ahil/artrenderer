fcd988b813059f627e4c3424c024c223aa34db6d
just-curious12 <manahil1078@hotmail.com>
2023-07-10 02:31:48 +0500

    uses axios now and error handling for 404s

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
-
+import axios from 'axios';
 import './App.css';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import ArtElement from './components/ArtElement';
+
 function App() {
-  const [art,setArt] = useState([])
-  const [artwork,setArtwork] = useState([])
+  const [art, setArt] = useState([]);
+
   useEffect(() => {
-    fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=1700&dateEnd=1800&q=African")
-    .then(res => res.json())
-    .then(data => setArt(data))
-  },[])
-  // useEffect(() => {
-  //     fetch("https://swapi.dev/api/people/1/")
-  //     .then(res => res.json())
-  //     .then(data => setArt(data))
-  //   },[])
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://collectionapi.metmuseum.org/public/collection/v1/search?medium=Quilts|Silk|Bedcovers&q=quilt'
+        );
+        const data = response.data;
+        setArt(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
 
-  
+    fetchData();
+  }, []);
 
-  const objects = art.objectIDs;
-
-
-  
-
-  const artElements = objects.map(item=> {
-    return (
-      <ArtElement key={item} objectID={item}  />
-    );
+  const objects = art.objectIDs || [];
+  const artElements = objects.map((item) => {
+    return <ArtElement key={item} objectID={item} />;
   });
-  //write me an alternative to the above map function that uses a for loop
-  // const artElements = [];
-  // console.log("i got here")
-  // for (let i = 1; i < 899311; i++) {
-  //   artElements.push(<ArtElement key={objects[i]} objectID={objects[i]} />);
-  // }
+
   return (
     <>
-    <div className="App">
-      {artElements}
-    </div>
+      <div className="App">{artElements}</div>
     </>
   );
 }
 
 export default App;
+
--- src/components/ArtElement.js
+++ src/components/ArtElement.js
@@ -1,21 +1,54 @@
-
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 export default function ArtElement(props) {
   const [artwork, setArtwork] = useState({});
-
+ const [error, setError] = useState(null);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //   fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/{props.objectID}`)
+  //     .then(res => {
+  //       if (!res.ok) {
+  //         throw new Error('Error retrieving artwork');
+  //       }
+  //       return res.json();
+  //     })
+  //     .then(data => setArtwork(data))
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //       setError(error);
+  //     });
+  // }, [props.objectID]);
   useEffect(() => {
-    fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + props.objectID)
-      .then(res => res.json())
-      .then(data => setArtwork(data))
-      .catch(error => console.error("Error:", error));
-  }, [props.objectID]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + props.objectID
+        );
+        const data = response.data;
+        setArtwork(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div className="art-element">Error: {error.message}</div>;
+  }
 
   return (
     <div className="art-element">
       {artwork.title && <h3>{artwork.title}</h3>}
       {artwork.artistDisplayName && <p>{artwork.artistDisplayName}</p>}
-      {artwork.primaryImage? <img src={artwork.primaryImage} width="300" height="300" alt=""></img> : <p>No image available</p>}
+      {artwork.primaryImage ? (
+        <img src={artwork.primaryImage} width="300" height="300" alt="" />
+      ) : (
+        <p>No image available</p>
+      )}
     </div>
   );
 }
+
