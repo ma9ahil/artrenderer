14cee8908c799c6b88ee11e461e47c1ec92a83a4
just-curious12 <manahil1078@hotmail.com>
2023-07-13 18:39:49 +0500

    set up react router, multiple pages setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import './App.css';
 import { useState, useEffect } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import ArtElement from './components/ArtElement';
+import Home from './pages/Home';
+import Art from './pages/Art';
 
 function App() {
   const [art, setArt] = useState([]);
@@ -30,11 +33,19 @@ function App() {
 
   return (
     <>
-      <nav className='navbar'>
+    <nav className='navbar'>
     <img src="./logo.png" height="100" alt=""></img>
-    <h3 className="nav-element">Home</h3></nav>
+    <div className='navbar-options'><h3 className="nav-element">Home</h3>
+    <h3 className="nav-element">About</h3>
+    <h3 className="nav-element">Collections</h3></div></nav>
     {/* <img  className="bgd" src="./image.jpg" height="600" width="100%"></img> */}
       <div className="App">{artElements}</div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/home" element={<Home/>}></Route>
+      <Route path="/about" element={<Art/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Art.js
@@ -0,0 +1,8 @@
+
+export default function Art(){
+    return(
+        <div>   
+            <h1>About us</h1>
+        </div>
+    );
+}
